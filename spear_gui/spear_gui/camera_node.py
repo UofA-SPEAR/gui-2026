e13592bbdaef0a7eca82b24a2d9f879540a30818
hhau1 <hhau1@ualberta.ca>
2026-03-05 18:21:53 -0700

    additional loading ui

--- spear_gui/spear_gui/camera_node.py
+++ spear_gui/spear_gui/camera_node.py
@@ -142,15 +142,17 @@ class RectDef:
         self.tweens = tween
 
 RECT_DEFS = [
-    RectDef(0.00, 0.00, 1.00, 0.50, QColor(20, 20, 20), False, [
-        Tween(0.00, 0.00, 1.00, 0.00, 0.30, 0.50, 'loaded', QEasingCurve.InOutQuint, None)]),
-    RectDef(0.00, 0.50, 1.00, 0.50, QColor(20, 20, 20), False, [
-        Tween(0.00, 1.00, 1.00, 0.00, 0.30, 0.50, 'loaded', QEasingCurve.InOutQuint, None)]),
-    RectDef(0.00, 0.00, 0.50, 1.00, QColor(20, 20, 20), False, [
-        Tween(0.00, 0.00, 0.00, 1.00, 0.30, 0.50, 'loaded', QEasingCurve.InOutQuint, None)]),
-    RectDef(0.50, 0.00, 0.50, 1.00, QColor(20, 20, 20), False, [
-        Tween(1.00, 0.00, 0.00, 1.00, 0.30, 0.50, 'loaded', QEasingCurve.InOutQuint, None)]),
+    # Background T/B/L/R
+    RectDef(0.00, 0.00, 1.00, 0.50, QColor(10, 10, 14), False, [
+        Tween(0.00, 0.00, 1.00, 0.00, 0.30, 1.00, 'loaded', QEasingCurve.OutQuint, None)]),
+    RectDef(0.00, 0.50, 1.00, 0.50, QColor(10, 10, 14), False, [
+        Tween(0.00, 1.00, 1.00, 0.00, 0.30, 1.00, 'loaded', QEasingCurve.OutQuint, None)]),
+    RectDef(0.00, 0.00, 0.50, 1.00, QColor(10, 10, 14), False, [
+        Tween(0.00, 0.00, 0.00, 1.00, 0.30, 1.00, 'loaded', QEasingCurve.OutQuint, None)]),
+    RectDef(0.50, 0.00, 0.50, 1.00, QColor(10, 10, 14), False, [
+        Tween(1.00, 0.00, 0.00, 1.00, 0.30, 1.00, 'loaded', QEasingCurve.OutQuint, None)]),
 
+    # Horizontal Corner TL/TR/BL/BR
     RectDef(0.00, -0.01, 0.50, 0, QColor(255, 255, 255), True, [
         Tween(0.30, 0.47, 0.20, 0.01, 0.30, 0.25, 'create', QEasingCurve.OutCirc, None),
         Tween(0.25, 0.45, 0.10, 0.01, 0.55, 0.50, 'create', QEasingCurve.InOutCirc, None),
@@ -172,6 +174,33 @@ RECT_DEFS = [
         Tween(0.75, 0.55 - 0.01, 0.00, 0.01, 0.30, 0.30, 'loaded', QEasingCurve.OutCirc, None),
     ]),
 
+    # Horizontal Thin Corner TL/TR/BL/BR
+    RectDef(0.30, 0.47 + 0.005, 0.20, 0.00, QColor(255, 255, 255), True, [
+        Tween(0.30, 0.47 + 0.005, 0.20, 0.005, 0.55, 0.00, 'create', QEasingCurve.InOutCirc, None),
+        Tween(0.345, 0.45 + 0.005, 0.10, 0.005, 0.55, 0.50, 'create', QEasingCurve.InOutCirc, None),
+        Tween(0.345, 0.45 + 0.005, 0.05, 0.005, 1.05, 1.75, 'create', QEasingCurve.InCirc, None),
+        Tween(0.345, 0.45 + 0.005, 0.00, 0.005, 0.00, 0.30, 'loaded', QEasingCurve.OutCirc, None),
+    ]),
+    RectDef(0.50, 0.47 + 0.005, 0.20, 0.00, QColor(255, 255, 255), True, [
+        Tween(0.50, 0.47 + 0.005, 0.20, 0.005, 0.55, 0.00, 'create', QEasingCurve.InOutCirc, None),
+        Tween(0.555, 0.45 + 0.005, 0.10, 0.005, 0.55, 0.50, 'create', QEasingCurve.InOutCirc, None),
+        Tween(0.605, 0.45 + 0.005, 0.05, 0.005, 1.05, 1.75, 'create', QEasingCurve.InCirc, None),
+        Tween(0.655, 0.45 + 0.005, 0.00, 0.005, 0.00, 0.30, 'loaded', QEasingCurve.OutCirc, None),
+    ]),
+    RectDef(0.30, 0.53 - 0.01, 0.20, 0.00, QColor(255, 255, 255), True, [
+        Tween(0.30, 0.53 - 0.01, 0.20, 0.005, 0.55, 0.00, 'create', QEasingCurve.InOutCirc, None),
+        Tween(0.345, 0.55 - 0.01, 0.10, 0.005, 0.55, 0.50, 'create', QEasingCurve.InOutCirc, None),
+        Tween(0.345, 0.55 - 0.01, 0.05, 0.005, 1.05, 1.75, 'create', QEasingCurve.InCirc, None),
+        Tween(0.345, 0.55 - 0.01, 0.00, 0.005, 0.00, 0.30, 'loaded', QEasingCurve.OutCirc, None),
+    ]),
+    RectDef(0.50, 0.53 - 0.01, 0.20, 0.00, QColor(255, 255, 255), True, [
+        Tween(0.50, 0.53 - 0.01, 0.20, 0.005, 0.55, 0.00, 'create', QEasingCurve.InOutCirc, None),
+        Tween(0.555, 0.55 - 0.01, 0.10, 0.005, 0.55, 0.50, 'create', QEasingCurve.InOutCirc, None),
+        Tween(0.605, 0.55 - 0.01, 0.05, 0.005, 1.05, 1.75, 'create', QEasingCurve.InCirc, None),
+        Tween(0.655, 0.55 - 0.01, 0.00, 0.005, 0.00, 0.30, 'loaded', QEasingCurve.OutCirc, None),
+    ]),
+
+    # Vertical Corner TL/BL/TR/BR
     RectDef(0.00, 0.00, 0.00, 0.50, QColor(255, 255, 255), True, [
         Tween(0.30, 0.47, 0.005625, 0.03, 0.30, 0.25, 'create', QEasingCurve.OutCirc, None),
         Tween(0.25, 0.45, 0.005625, 0.02, 0.55, 0.50, 'create', QEasingCurve.InOutCirc, None),
@@ -193,19 +222,21 @@ RECT_DEFS = [
         Tween(0.75 - 0.005625, 0.53, 0.005625, 0.00, 0.60, 0.30, 'loaded', QEasingCurve.OutCirc, None),
     ]),
 
+    # Large Squares L/R
     RectDef(0.50, 0.50, 0.00, 0.00, QColor(180, 180, 180), True, [
         Tween(0.50 - 10/1920, 0.50 - 10/1080, 20/1920, 20/1080, 0.00, 0.30, 'create', QEasingCurve.OutCirc, None),
         Tween(0.23, 0.50 - 10/1080, 20/1920, 20/1080, 0.30, 1.00, 'create', QEasingCurve.OutBack, None),
         Tween(0.25, 0.50 - 10/1080, 0.005625, 20/1080, 0.30, 0.30, 'loaded', QEasingCurve.OutCirc, QColor(255, 255, 255)),
-        Tween(0.25, 0.50, 0.005625, 0, 0.60, 0.30, 'loaded', QEasingCurve.OutCirc, None),
+        Tween(0.25, 0.50, 0.005625, 0, 0.90, 0.30, 'loaded', QEasingCurve.OutCirc, None),
     ]),
     RectDef(0.50, 0.50, 0.00, 0.00, QColor(180, 180, 180), True, [
         Tween(0.50 - 10/1920, 0.50 - 10/1080, 20/1920, 20/1080, 0.00, 0.30, 'create', QEasingCurve.OutCirc, None),
         Tween(0.77 - 20/1920, 0.50 - 10/1080, 20/1920, 20/1080, 0.30, 1.00, 'create', QEasingCurve.OutBack, None),
         Tween(0.75 - 0.005625, 0.50 - 10/1080, 0.005625, 20/1080, 0.30, 0.30, 'loaded', QEasingCurve.OutCirc, QColor(255, 255, 255)),
-        Tween(0.75 - 0.005625, 0.50, 0.005625, 0, 0.60, 0.30, 'loaded', QEasingCurve.OutCirc, None),
+        Tween(0.75 - 0.005625, 0.50, 0.005625, 0, 0.90, 0.30, 'loaded', QEasingCurve.OutCirc, None),
     ]),
 
+    # Small Squares TL/TR/BL/BR
     RectDef(0.50, 0.50, 0.00, 0.00, QColor(180, 180, 180), True, [
         Tween(0.50 - 5/1920, 0.50 - 5/1080, 10/1920, 10/1080, 0.00, 0.30, 'create', QEasingCurve.OutCirc, None),
         Tween(0.50 - 70/1920, 0.50 - 70/1080, 10/1920, 10/1080, 0.30, 1.00, 'create', QEasingCurve.OutBack, None),
@@ -231,6 +262,7 @@ RECT_DEFS = [
         Tween(0.50, 0.50, 0.00, 0.00, 0.60, 0.30, 'loaded', QEasingCurve.OutCirc, None),
     ]),
 
+    # Progress Bar Outline T/B/L/R
     RectDef(0.50, 0.50, 0.00, 0.00, QColor(120, 120, 120), True, [
         Tween(0.25 + 0.005625 * 2, 0.45 + 0.01 * 2, 0.05625 - 0.005625 * 2 * 2, 0.0025, 0.30, 0.70, 'create', QEasingCurve.InOutQuad, None),
         Tween(0.25 + 0.005625 * 2, 0.45 + 0.01 * 2, 0.50 - 0.005625 * 2 * 2, 0.0025, 1.00, 1.00, 'create', QEasingCurve.InOutQuad, None),
@@ -253,6 +285,7 @@ RECT_DEFS = [
         Tween(0.25 + 0.005625 * 2 + 0.50 - 0.005625 * 2 * 2 - 0.00140625, 0.45 + 0.01 * 2, 0.00140625, 0.00, 0.30, 0.30, 'loaded', QEasingCurve.OutCirc, QColor(255, 255, 255)),
     ]),
     
+    # Progress Bar
     RectDef(0.50, 0.45 + 0.01 * 3, 0.00, 0.10 - 0.01 * 3 * 2, QColor(255, 255, 255), True, [
         Tween(0.25 + 0.005625 * 3, 0.45 + 0.01 * 3, 0.05625 - 0.005625 * 3 * 2, 0.10 - 0.01 * 3 * 2, 0.30, 0.70, 'create', QEasingCurve.InOutQuad, None),
         Tween(0.25 + 0.005625 * 3, 0.45 + 0.01 * 3, 0.50 - 0.005625 * 3 * 2, 0.10 - 0.01 * 3 * 2, 1.00, 1.80, 'create', QEasingCurve.InOutCirc, None),
