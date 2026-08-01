d305f083826f32125aaef4cf1a9bcb53e53c7512
nathan-correia <nathanc54@shaw.ca>
2026-08-01 14:08:34 -0600

    png fix

--- spear_gui/setup.py
+++ spear_gui/setup.py
@@ -8,11 +8,14 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
+    package_data={
+        package_name: ['*.png', '*.ttf'],
+    },
+    include_package_data=True,
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('lib/' + package_name, ['spear_gui/Oxanium-Regular.ttf', 'spear_gui/Oxanium-SemiBold.ttf']),
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.py'))),
     ],
     install_requires=['setuptools'],
